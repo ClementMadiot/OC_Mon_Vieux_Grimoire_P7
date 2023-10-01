a8a352a37c55185ebddc8e929fe526f71d1f61f7
ClementMadiot <clementmadiot09@gmail.com>
2023-10-01 17:27:35 +0200

    decodedToken.useId

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    // console.log(req.headers.authorization);
+    console.log(req.headers.authorization);
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
+    const userId = decodedToken.useId;
     req.auth = {
       userId: userId
     };
